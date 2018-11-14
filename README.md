1b76a697cf1084d3040a64ac6b7471c3b5c24428
devadvance <encryptstream@gmail.com>
2018-11-14 00:53:16 -0500

    Added first rough version

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Transit Techies Homepage
 
 Work-in-progress homepage for the Transit Techies meetup group and all things related to it!
+
+Based on the simple blog AMP template.
---
+++ codefunction.js
@@ -0,0 +1,64 @@
+const https = require('https');
+/**
+ * Responds to any HTTP request with the appropriate events
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.getEvents = (req, res) => {
+  let errorMessage = {error: 'Error retrieving data from meetup'};
+  let resultList = [];
+  res.set('Content-Type', 'application/json');
+
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+  } else {
+    res.set('Access-Control-Allow-Origin', '*');
+  }
+
+  let meetupUrl = ''; // Set as empty for now
+
+  if (req.query.status === 'past') {
+    meetupUrl = '<URL_GOES_HERE>';
+  } else if (req.query.status === 'upcoming') {
+    meetupUrl = '<URL_GOES_HERE>';
+  } else { // Get both past and upcoming
+    meetupUrl = '<URL_GOES_HERE>';
+  }
+  
+  https.get(meetupUrl, (resp) => {
+    let data = '';
+
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    resp.on('end', () => {
+      parsedData = JSON.parse(data);
+      for (i = 0; i < parsedData.length; i++) { 
+        var eventInstance = {
+          name: parsedData[i].name,
+          status: parsedData[i].status,
+          link: parsedData[i].link,
+          local_date: parsedData[i].local_date,
+          local_time: parsedData[i].local_time,
+          venue_name: parsedData[i].venue.name,
+          venue_address_1: parsedData[i].venue.address_1,
+          venue_city: parsedData[i].venue.city
+        };
+
+        resultList.push(eventInstance);
+      }
+
+      var finalResult = {items: resultList};
+      res.status(200).send(JSON.stringify(finalResult));
+
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    res.status(500).send(JSON.stringify(errorMessage));
+  });
+};
